67a727bd20fcab51b99b06e69669dc89327c8f3f
Jeywoods <VladSagaydak1@gmail.com>
2026-03-11 15:59:51 +0300

    implementation of the calculator interface

--- app/src/main/java/mmcs/assignment3_calculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/mmcs/assignment3_calculator/viewmodel/CalculatorViewModel.kt
@@ -6,27 +6,59 @@ import androidx.databinding.ObservableField
 class CalculatorViewModel: BaseObservable(), Calculator {
     override var display = ObservableField<String>()
 
+    private var firstNumber: Double = 0.0
+    private var secondNumber: Double = 0.0
+    private var previousOperation: Operation? = null
+
+
     override fun addDigit(dig: Int) {
-        TODO("Not yet implemented")
+        val currentNumber = display.get() ?: "0"
+        display.set(if(currentNumber == "0") "$dig" else currentNumber + dig)
     }
 
     override fun addPoint() {
-        TODO("Not yet implemented")
+        val currentNumber = display.get() ?: "0"
+        if(!currentNumber.contains("."))
+            display.set("$currentNumber.")
     }
 
     override fun addOperation(op: Operation) {
-        TODO("Not yet implemented")
+        if(previousOperation != null)
+            compute()
+        firstNumber = display.get()?.toDoubleOrNull() ?: 0.0
+        previousOperation = op
+        display.set("0")
     }
 
     override fun compute() {
-        TODO("Not yet implemented")
+        secondNumber = display.get()?.toDoubleOrNull() ?: 0.0
+        if (previousOperation == Operation.DIV && secondNumber == 0.0) {
+            display.set("Не определен")
+            previousOperation = null
+            return
+        }
+        val result = when(previousOperation){
+            Operation.ADD -> firstNumber + secondNumber
+            Operation.SUB -> firstNumber - secondNumber
+            Operation.MUL -> firstNumber * secondNumber
+            Operation.DIV -> firstNumber / secondNumber
+            Operation.PERC -> firstNumber * secondNumber / 100
+            Operation.NEG -> -secondNumber
+            null -> secondNumber
+        }
+        display.set(result.toString())
+        firstNumber = result
+        previousOperation = null
     }
 
     override fun clear() {
-        TODO("Not yet implemented")
+        display.set("0")
     }
 
     override fun reset() {
-        TODO("Not yet implemented")
+        clear()
+        firstNumber = 0.0
+        secondNumber = 0.0
+        previousOperation = null
     }
 }
